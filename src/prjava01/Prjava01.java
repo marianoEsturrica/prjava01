600f8e54eb3c7e9283df0063b6c2e0c23131e7c2
marianoEsturrica <50147970+marianoEsturrica@users.noreply.github.com>
2019-05-01 09:26:25 +0200

    Versió 0.2 del projecte prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -10,6 +10,7 @@ public class Prjava01{
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
+ 
         File f = new File("fitxer.html");
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
             bw.write("<html>");bw.newLine();
@@ -22,6 +23,7 @@ public class Prjava01{
             bw.write("    Nova p&agrave;gina web");bw.newLine();
             bw.write("  </body>");bw.newLine();
             bw.write("</html>");bw.newLine();
+            bw.write(" <h1>DAW2 m08uf4eac1</h1>");bw.newLine();
             bw.close();
 } }
 }
